ad81cf5d2ebef53285fe5b125bbce248d600c635
Pavel Chukhlebov <indisguiseweare@gmail.com>
2017-12-28 22:26:37 +0500

    up

---
+++ lfsr/lfsr/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lfsr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Считывание размера регистра
+            Console.Write("Введите размер регистра: ");
+            int size = Int32.Parse(Console.ReadLine());
+
+            // Введение стартового состояния
+            bool[] start = new bool[size];
+            for(int i = 0; i < size; i++)
+            {
+                Console.Write("Введите " + (i + 1) + " элемент: ");
+                start[i] = Convert.ToBoolean(Convert.ToByte(Console.ReadLine()));
+            }
+
+            // Считывание полинома по длине регистра
+            string[] polynoms = File.ReadAllLines(size.ToString() + ".txt", Encoding.Default);
+
+            // Запись полиномов в битовой форме
+            Console.WriteLine("Полиномы:");
+            for (int i = 0; i < polynoms.Length; i++)
+                Console.WriteLine(Convert.ToString(Convert.ToInt32(polynoms[i], 16), 2));
+
+            // Вводим выбранный полином
+            Console.Write("Введите полином:");
+            string pol = Console.ReadLine();
+
+            // Объект выходного файла
+            StreamWriter outputFile;
+            
+            // Объект класса регистра
+            lfsr l = new lfsr(size, pol, start);
+
+            // Прохождение сдвигов
+            while (true)
+            {
+                Console.Write("Введите количество итераций: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                outputFile = new StreamWriter("output.txt", false);
+                for (int i = 0; i < n; i++)
+                {
+                    l.next();
+                    outputFile.WriteLine(l.ToString());
+                }
+                Console.WriteLine("Выполнено");
+                outputFile.Close();
+            }
+        }
+    }
+}
---
+++ lfsr/lfsr/lfsr.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lfsr
+{
+    class lfsr
+    {
+        private int regLen { get; set; }
+        public int size { get; set; }
+        private string pol;
+
+        public bool[] reg;
+        private bool[] regCopy;
+
+        public lfsr(int size, string polynom, bool[] start)
+        {
+            regLen = size;
+            this.size = regLen;
+            pol = polynom;
+            reg = new bool[size];
+            Array.Copy(start, reg, size);
+        }
+
+        private bool f()
+        {
+            bool res = true;
+            int j = 0;
+
+            for (var i = 0; i < regLen; i++)
+            {
+                if (pol[i] == '1')
+                {
+                    res = reg[i];
+                    j = i;
+                    break;
+                }
+            }
+
+            for (int i = j + 1; i < regLen; i++)
+                if (pol[i] == '1')
+                    res = res ^ Convert.ToBoolean(reg[i]);
+            return res;
+        }
+
+        public void next()
+        {
+            regCopy = new bool[size];
+            regCopy[0] = f();
+            Array.Copy(reg, 0, regCopy, 1, size - 1);
+            reg = new bool[size];
+            reg = regCopy;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            for (int i = 0; i < size; i++)
+                if (reg[i] == true)
+                    str += "1";
+                else
+                    str += "0";
+            return str;
+        }
+    }
+}
